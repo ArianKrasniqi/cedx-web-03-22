7d242a167e154608fc938df783fe12c96cb0fa91
ArianKrasniqi <ariiankrasniqi@gmail.com>
2022-05-31 03:05:52 +0200

    Day 19 - JS

---
+++ Day19-[30-05-22]/script.js
@@ -0,0 +1,28 @@
+const name = "Arian";
+
+let lastname = "Krasniqi"
+
+const city = "Peje"
+
+center = "cedx"
+
+// window.alert('This is my first Alert')
+// alert(lastname)
+
+console.log(city, name, lastname, center)
+
+const clubOfCity = "KB Peja" // Camel Case -> Best practice
+const club_of_city = "KB Peja" // Bad Practice
+// const club-of-city = "KB Peja" // Can't use it
+
+const clubOfCity2 = "---"
+const $clubOfCity2 = "---"
+const _clubOfCity2 = "---"
+// const 2clubOfCity2 = "---"  // We can't start a variable name with a number
+
+// const let = "Arian" // Can't use keywords as varible names
+
+const Name = "Dion"
+
+console.log(name)
+console.log(Name)
---
+++ Day19-[30-05-22]/second-script.js
@@ -0,0 +1,51 @@
+// alert('Blaa')
+
+let milk = 1.5, chocolate = 1.75, total;
+
+// milk = 1.5;
+// chocolate = 1.75;
+
+total = milk + chocolate;
+console.log(total);
+
+const trainingCenter = "cedX";
+
+let sentence = trainingCenter + " " + "course";
+console.log(sentence)
+
+let firstName, lastName, fullName;
+firstName = "John";
+lastName = "Doe";
+fullName = firstName + " " + lastName;
+console.log(fullName);
+
+let sum = 0;
+console.log(sum)
+sum = sum + 5;
+console.log(sum)
+sum = sum + 10;
+console.log(sum)
+sum = sum - 13;
+console.log(sum)
+
+sum += 5;
+console.log(sum)
+
+sum *= 5;
+console.log(sum)
+
+sum /= 5;
+console.log(sum)
+
+let newSentence = "Blaa" // We are declaring a variable there
+/* console.log(newSentence)
+newSentence = newSentence + " adding more Bla"
+console.log(newSentence) */
+
+newSentence += " adding SHORTHAND Bla"
+console.log(newSentence)
+
+const name = 'Arian'
+const lastname = "Krasniqi"
+
+const sentence = 'It"s me'
